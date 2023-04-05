0e559941e7a532b5c7ca96a8f691fb7d875c572c
Mohammed Amaan Khan <amaanrkhan9@gmail.com>
2023-04-06 00:34:25 +0530

    feat: add login/register
    added the register functionality which also stores the users to
    localStorge and then at login matches the details and logs the user in
    with proper validations and feedbacks.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("register").style.display="none";
+document.getElementById("register").style.display = "none";
 
 function checkEmail(email) {
   var validRegex =
@@ -8,24 +8,35 @@ function checkEmail(email) {
   else return false;
 }
 
-function showRegister(){
-    document.getElementById("login").style.display="none";
-    document.getElementById("register-prompt").style.display="none";
-    document.getElementById("register").style.display="block";
+function showRegister() {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("register-prompt").style.display = "none";
+  document.getElementById("login-prompt").style.display = "block";
+  document.getElementById("register").style.display = "block";
+}
+
+function showLogin() {
+  document.getElementById("register").style.display = "none";
+  document.getElementById("login-prompt").style.display = "none";
+  document.getElementById("register-prompt").style.display = "block";
+  document.getElementById("login").style.display = "block";
 }
 
 function register(e) {
   e.preventDefault();
   let email = document.getElementById("reg");
+  let username = document.getElementById("reg-username");
+  let firstname = document.getElementById("reg-firstname");
+  let lastname = document.getElementById("reg-lastname");
+  let gender = document.getElementById("reg-gender");
+  let department = document.getElementById("reg-dept");
   let pass1 = document.getElementById("pass1");
   let pass2 = document.getElementById("pass2");
-  let department = document.getElementById("reg-dept");
 
   // function selectDept(dept){
   //     department = dept;
   // }
 
-  console.log(email.value, pass1.value, pass2.value, department.value);
   if (pass1.value !== pass2.value) {
     alert("Passwords doesn't match");
     pass1.value = "";
@@ -45,23 +56,77 @@ function register(e) {
       ...JSON.parse(localStorage.getItem("users") || "[]"),
       {
         email: email.value,
-        password: pass1.value,
+        username: username.value,
+        firstname: firstname.value,
+        lastname: lastname.value,
+        gender: gender.value,
         department: department.value,
+        password: pass1.value,
       },
     ])
   );
 
+  console.log(
+    email.value,
+    username.value,
+    firstname.value,
+    lastname.value,
+    gender.value,
+    department.value,
+    pass1.value,
+    pass2.value
+  );
+
   alert("You are registered successfully");
+  email.value = "";
+  username.value = "";
+  firstname.value = "";
+  lastname.value = "";
+  gender.value = "";
   pass1.value = "";
   pass2.value = "";
-  email.value = "";
 }
 
 function login(e) {
-//   e.preventDefault();
+  e.preventDefault();
   let users = Array.from(JSON.parse(localStorage.getItem("users")));
-  console.log('users :>> ', JSON.stringify(users));
-  let email = document.getElementById("login");
-  let pass = document.getElementById("pass");
-  let department = document.getElementById("login-dept");
+  // console.log("users :>> ", users);
+  let email = document.getElementById("login-email");
+  let pass = document.getElementById("password");
+  let currentUser;
+  let usernameMatched = false;
+  users.forEach((user) => {
+    if (user.email === email.value || user.username === email.value) {
+      usernameMatched = true;
+      if (user.password === pass.value) {
+        currentUser = user;
+        alert(`You are logged in as ${email.value}`);
+      }
+    }
+  });
+
+  if (currentUser == null) {
+    if (usernameMatched) {
+      alert("Password doesn't the username!");
+    } else {
+      alert("Wrong username or password!");
+    }
+    return;
+  }
+
+  email.value = "";
+  pass.value = "";
+
+  localStorage.setItem(
+    "loggedInUser",
+    JSON.stringify({
+      email: currentUser.email,
+      username: currentUser.username,
+      firstname: currentUser.firstname,
+      lastname: currentUser.lastname,
+      gender: currentUser.gender,
+      department: currentUser.department,
+      // password: pass1.value,
+    })
+  );
 }
