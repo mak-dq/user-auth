151dac871fab1ac5d9789b0eb8dc467a4e651382
Amaan <amkhan@deqode.com>
2023-04-06 18:11:23 +0530

    feat: add show-user functionality

--- script.js
+++ script.js
@@ -8,10 +8,104 @@ function checkEmail(email) {
   else return false;
 }
 
-function showRegister(){
-    document.getElementById("login").style.display="none";
-    document.getElementById("register-prompt").style.display="none";
-    document.getElementById("register").style.display="block";
+function showDetails(user) {
+  // console.log(user);
+  document.getElementById("users").style.display = "none";
+  document.getElementById("user-details").style.display = "flex";
+  let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+
+  document.getElementById("go-back").style.display =
+    loggedInUser.department === "sales" ? "none" : "inline";
+
+  document.getElementById(
+    "logged-in-as-2"
+  ).innerHTML = `Logged in as: ${loggedInUser.username} (${loggedInUser.email})`;
+
+  document.getElementById("user-details-email").innerHTML = user.email;
+  document.getElementById("user-details-username").innerHTML = user.username;
+  document.getElementById("user-details-firstname").innerHTML = user.firstname;
+  document.getElementById("user-details-lastname").innerHTML = user.lastname;
+  document.getElementById("user-details-gender").innerHTML = user.gender;
+  document.getElementById("user-details-department").innerHTML =
+    user.department;
+}
+
+function showToAdmins(userlist, users) {
+  let i = 1;
+  users.forEach((user) => {
+    const userElement = document.createElement("tr");
+    userElement.innerHTML = `
+              <td class="tg-0lax">${i}</td>
+              <td class="tg-0lax">${user.email}</td>
+              <td class="tg-0lax">${user.username}</td>
+              <td class="tg-0lax">${user.firstname}</td>
+              <td class="tg-0lax">${user.lastname}</td>
+              <td class="tg-0lax">${user.gender}</td>
+              <td class="tg-0lax">${user.department}</td>
+    `;
+    userElement.setAttribute(
+      "onclick",
+      `showDetails((${JSON.stringify(user)}))`
+    );
+    userlist.appendChild(userElement);
+    i++;
+  });
+}
+
+function showToOperations(userlist, users, currentUser) {
+  let i = 1;
+  users.forEach((user) => {
+    if (user.department === "admin") return;
+    else if (
+      user.email !== currentUser.email &&
+      user.department === "operations"
+    )
+      return;
+    const userElement = document.createElement("tr");
+    userElement.innerHTML = `
+              <td class="tg-0lax">${i}</td>
+              <td class="tg-0lax">${user.email}</td>
+              <td class="tg-0lax">${user.username}</td>
+              <td class="tg-0lax">${user.firstname}</td>
+              <td class="tg-0lax">${user.lastname}</td>
+              <td class="tg-0lax">${user.gender}</td>
+              <td class="tg-0lax">${user.department}</td>
+    `;
+    userElement.setAttribute(
+      "onclick",
+      `showDetails((${JSON.stringify(user)}))`
+    );
+    userlist.appendChild(userElement);
+    i++;
+  });
+}
+
+function showToSales(currentUser) {
+  showDetails(currentUser);
+  document.getElementById("user-details-heading").innerHTML = "Your details";
+}
+
+function goBack() {
+  document.getElementById("users").style.display = "block";
+  document.getElementById("user-details").style.display = "none";
+}
+
+function showUsers(currentUser, users) {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("users").style.display = "block";
+  document.getElementById(
+    "logged-in-as-1"
+  ).innerHTML = `Logged in as: ${currentUser.username} (${currentUser.email})`;
+
+  // console.log(currentUser, users);
+  const userlist = document.getElementById("users-list");
+  if (currentUser.department === "admin") {
+    showToAdmins(userlist, users);
+  } else if (currentUser.department === "operations") {
+    showToOperations(userlist, users, currentUser);
+  } else {
+    showToSales(currentUser);
+  }
 }
 
 function register(e) {
